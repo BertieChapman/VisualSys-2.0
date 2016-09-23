cf1fef98c1bdda45c5efcdd34cd8f3f3ae1d57a4
Bertrand <bertrand_chapman@hotmail.com>
2016-09-23 10:37:04 +1000

    added vis dependency and created client library folder

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Bertie on 22/09/2016.
+ */
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});

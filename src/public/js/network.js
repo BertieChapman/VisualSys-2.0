7b1869d38ab140fc8be1fd355bc1b032c817723c
Bertrand <bertrand_chapman@hotmail.com>
2016-09-27 10:01:30 +1000

    editor modes implemented

--- src/public/js/network.js
+++ src/public/js/network.js
@@ -2,42 +2,30 @@
  * Created by Bertie on 23/09/2016.
  */
 
-var id = 100;
-
-var nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1'},
-    {id: 2, label: 'Node 2'},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'}
-]);
-
-// create an array with edges
-var edges = new vis.DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5}
-]);
-
+var id = 1;
 
 function initNetworkArea(){
     var netArea = document.getElementById('network_area');
 
     dataSet = {
-        nodes: nodes,
-        edges: edges
+        nodes: new vis.DataSet,
+        edges: new vis.DataSet
     };
     var options = {
+        physics: {
+            enabled: false,
+        }
     };
     // initialize network
     network = new vis.Network(netArea, dataSet, options);
+
 }
 
 function addNode(pos){
     dataSet.nodes.add({id :id, text : "test", x : pos.x , y : pos.y});
     id++;
     network.redraw();
+
 }
 
 
